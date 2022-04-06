88a305623221698c1c94b6638195f9de2df9eee8
reindeer002 <lsj000424@gmail.com>
2022-04-06 17:52:29 +0000

    ✨ feat: add database schema

--- warroombot/models.py
+++ warroombot/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    studentnum = models.CharField(max_length=8)
+    starttime = models.TimeField(auto_now=False, auto_now_add=False) #%H:%M
+    endtime = models.TimeField(auto_now=False, auto_now_add=False) #%H:%M
+    date = models.DateField(auto_now=False, auto_now_add=False) #%Y-%m-%d
+    count = models.IntegerField()
+    name = models.TextField()
+    content = models.TextField()
+
+class Forbid(models.Model):
+    starttime = models.TimeField(auto_now=False, auto_now_add=False) #%H:%M
+    endtime = models.TimeField(auto_now=False, auto_now_add=False) #%H:%M
+    dayofweek = models.CharField(max_length=3)
+    
+class Calendar(models.Model):
+    calendarpic = models.ImageField(upload_to='images/',blank=True, null=True)
+    inittime = models.DateTimeField()
