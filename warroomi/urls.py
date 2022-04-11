545c78aad318fdcbaa2943f425faededf9bcaffe
reindeer002 <lsj000424@gmail.com>
2022-04-11 07:47:41 +0000

    🎉 refactor: start google web hook project

--- warroomi/urls.py
+++ warroomi/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('warroombot.urls')),
+    path('webhook/', include('googlewebhook.urls')),
     path('admin/', admin.site.urls),
 ]
