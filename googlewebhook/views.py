110a1d44c72709deb978cc15396d8dec2f8140aa
reindeer002 <lsj000424@gmail.com>
2022-04-11 07:48:20 +0000

    🎉 refactor: start google web hook project

---
+++ googlewebhook/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+import logging
+
+# Create your views here.
+def googledrive(request):
+    return "True"
